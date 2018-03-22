1156a391b061136582fe08fd1beceddac43fbba8
Federico Pazos <fede.pazos95@gmail.com>
2018-03-22 15:02:05 -0300

    modified server.js to serve files

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 5000;
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 require('./routes/items')(app);
 
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
